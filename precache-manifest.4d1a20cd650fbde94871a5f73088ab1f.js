838569bbd472efad6c31dec197ece241881369f2
khantly101 <khantuanly@gmail.com>
2019-06-29 09:50:50 -0700

    Updates

---
+++ precache-manifest.4d1a20cd650fbde94871a5f73088ab1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb3123ac13ca1a88b45e6f6adcd23f3",
+    "url": "/watch_party_fake/index.html"
+  },
+  {
+    "revision": "0646f02c95268493b028",
+    "url": "/watch_party_fake/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/watch_party_fake/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0646f02c95268493b028",
+    "url": "/watch_party_fake/static/js/main.d8e7d465.chunk.js"
+  },
+  {
+    "revision": "a1f4de0b607ae9cbc1bd",
+    "url": "/watch_party_fake/static/js/runtime~main.1e77be6f.js"
+  }
+]);
